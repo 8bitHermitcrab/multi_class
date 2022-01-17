b2f54ad274eaf82e230354dc717f8fa7567be1ae
8bithermitcrab <kij517@naver.com>
2022-01-17 15:51:14 +0900

    220117

--- 220117/test.py
+++ 220117/test.py
@@ -1,4 +1,5 @@
 #test.py
+""" # '''도 주석 가능
 import sys
 
 #print(sys.path)
@@ -6,3 +7,36 @@ import sys
 sys.path.append('/Users/kij/workspace/220117/mod')
 
 print(sys.path)
+"""
+
+#FileNotFoundError: [Errno 2] No such file or directory: 'xxx.txt'
+#f = open('xxx.txt', 'r')
+
+#ZeroDivisionError: division by zero
+#print(4/0)
+
+#IndexError: list index out of range
+#a = [1, 2, 3]
+#print(a[4])
+
+#에러가 나도 끝까지 실행(try, except)
+from ast import Index
+
+
+try:
+    a = [1, 2, 3]
+    print(a[4])
+    4/0
+# except ZeroDivisionError as e:
+#     print(e)
+# except IndexError as e:
+#     print(e)
+
+# except (ZeroDivisionError, IndexError) as e:
+#     print(e)
+
+#Exception은 부모 클래스, ZeroDivisionError, IndexError는 자식 클래스
+except Exception as e:
+    print(e)
+
+print('Hello World')
