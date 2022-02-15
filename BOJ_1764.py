bbe5264f0fb65a1b68566c6c90f172614de7350b
8bithermitcrab <kij517@naver.com>
2022-02-16 00:43:56 +0900

    220215

--- BOJ_1764.py
+++ BOJ_1764.py
@@ -1,11 +1,39 @@
 # 듣보잡
-N, M = input().split()
-N_list = []
-M_list = []
+N, M = map(int, input().split())
 
-for _ in range(N):
-input()
+a = set()
+b = set()
 
-for _ in range(M):
-input()
+for i in range(N):
+    a.add(input())
 
+for i in range(M):
+    b.add(input())
+
+# print(f'a = {a}')
+# print(f'b = {b}')
+# print(f'list(a & b) = {list(a & b)}')
+result = sorted(list(a & b))
+# print(f'result = {result}')
+print(len(result))
+
+for i in result:
+    print(i)
+
+
+'''
+# n_value = [input() for _ in range(N)]
+# m_value = [input() for _ in range(M)]
+
+# nm = n_value + m_value
+
+# 중복값 제거
+nm_set = set(nm)
+nm_del = list(nm_set)
+
+# print(len(nm)-len(nm_del))
+
+print(nm - nm_del)
+
+https://wook-2124.tistory.com/476
+'''
