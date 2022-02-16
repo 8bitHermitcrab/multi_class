7c8d58602bd9d936094563815595dda7a60ec0ca
8bithermitcrab <kij517@naver.com>
2022-02-17 00:42:35 +0900

    220216

--- BOJ_1764.py
+++ BOJ_1764.py
@@ -1,6 +1,24 @@
 # 듣보잡
 N, M = map(int, input().split())
 
+a = {input() for _ in range(N)}
+b = {input() for _ in range(M)}
+
+# print(f'a = {a}')
+# print(f'b = {b}')
+# print(f'list(a & b) = {list(a & b)}')
+# result = sorted(list(a & b))
+# print(f'result = {result}')
+result = sorted(a & b)
+print(len(result))
+
+for i in result:
+    print(i)
+
+
+'''
+N, M = map(int, input().split())
+
 a = set()
 b = set()
 
@@ -19,7 +37,7 @@ print(len(result))
 
 for i in result:
     print(i)
-
+'''
 
 '''
 # n_value = [input() for _ in range(N)]
--- BOJ_5622.py
+++ BOJ_5622.py
@@ -1,1 +1,39 @@
-# 
+# 다이얼
+
+# 1에 2초 
+'''
+2ABC는 +1초 = 3초
+3DEF 4
+4GHI 5
+5JKL 6
+6MNO 7
+7PQRS 8
+8TUV 9
+9WXYZ 10
+0 11
+'''
+
+dial = {'A':3, 'B':3, 'C':3, 
+'D':4, 'E':4, 'F':4,
+'G':5, 'H':5, 'I':5,
+'J':6, 'K':6, 'L':6,
+'M':7, 'N':7, 'O':7,
+'P':8, 'Q':8, 'R':8, 'S':8,
+'T':9, 'U':9, 'V':9,
+'W':10, 'X':10, 'Y':10, 'Z':10}
+
+word = input().upper()
+
+#print(word[0])
+count = 0  
+
+for i in range(len(word)):
+    for key in dial:
+        if word[i] == key:
+            count = count + dial[key]
+
+print(count)
+
+'''
+https://j-remind.tistory.com/76
+'''
